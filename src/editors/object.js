ecd712177f4ceb863bb7fcdfc958cb13c87eda24
Benjamin Urban <benjamin.urban@sueddeutsche.de>
2015-04-02 13:09:44 +0200

    Add option hide_title

--- src/editors/object.js
+++ src/editors/object.js
@@ -288,6 +288,11 @@ JSONEditor.defaults.editors.object = JSONEditor.AbstractEditor.extend({
       this.header = document.createElement('span');
       this.header.textContent = this.getTitle();
       this.title = this.theme.getHeader(this.header);
+
+      if(this.schema.options && typeof this.schema.options.hide_title !== "undefined") {
+        this.title.style.display = 'none';
+      }
+
       this.container.appendChild(this.title);
       this.container.style.position = 'relative';
       
